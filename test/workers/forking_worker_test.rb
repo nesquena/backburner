8500dceaba5871307dbb70403c0c7783c1a397c0
Daniel Farrell <dfarrell@wmci.com>
2012-12-23 16:55:04 -0500

    Add comments to forking work and exit cleanly. Add a test.

---
+++ test/workers/forking_worker_test.rb
@@ -0,0 +1,35 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+describe "Backburner::Workers::Forking module" do
+
+  before do
+    Backburner.default_queues.clear
+    @worker_class = Backburner::Workers::Forking
+  end
+
+  describe "for fork_one_job method" do
+
+    it "should fork, reconnect, work job, and exit" do
+
+      clear_jobs!("foo.bar")
+      @worker_class.enqueue TestJob, [1, 2], :queue => "foo.bar"
+
+      fake_pid = 45
+      Process.expects(:fork).returns(fake_pid) do |&block|
+        Connection.expects(:new).with(Backburner.configuration.beanstalk_url)
+        @worker_class.any_instance.expects(:work_one_job)
+        @worker_class.any_instance.expects(:coolest_exit)
+        block.call
+      end
+      Process.expects(:wait).with(fake_pid)
+
+      silenced(2) do
+        worker = @worker_class.new('foo.bar')
+        worker.prepare
+        worker.fork_one_job
+      end
+    end
+
+  end # fork_one_job
+
+end
