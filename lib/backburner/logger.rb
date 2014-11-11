f9cba3038a365b25a57e1430afe2f784c3b9843b
Andrew Tribone <atribone@gmail.com>
2014-11-10 18:24:55 -0800

    log_job_begin/log_job_end taks job to allow more useful logging

--- lib/backburner/logger.rb
+++ lib/backburner/logger.rb
@@ -8,18 +8,18 @@ module Backburner
     end
 
     # Print out when a job is about to begin
-    def log_job_begin(name, args)
-      log_info "Work job #{name} with #{args.inspect}"
+    def log_job_begin(job)
+      log_info "Work job #{job.name} with #{job.args.inspect}"
       @job_started_at = Time.now
     end
 
     # Print out when a job completed
     # If message is nil, job is considered complete
-    def log_job_end(name, message = nil)
+    def log_job_end(job, message = nil)
       ellapsed = Time.now - job_started_at
       ms = (ellapsed.to_f * 1000).to_i
       action_word = message ? 'Finished' : 'Completed'
-      log_info("#{action_word} #{name} in #{ms}ms #{message}")
+      log_info("#{action_word} #{job.name} in #{ms}ms #{message}")
     end
 
     # Returns true if the job logging started
